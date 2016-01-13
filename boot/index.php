46b3f9b0614d7806d2c61c129ff8c9574ed456ea
Cyril DUSSERT <cyril.dussert@etu.univ-savoie.fr>
2016-01-13 13:40:05 +0100

    Configuring Carousel
    Using Bootstrap natvie Carousel

--- boot/index.php
+++ boot/index.php
@@ -13,3 +13,43 @@ include 'header.php';
   	</ul>
   </div>
 </nav>
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+          <h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+          <h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+          <h3>Caption Text</h3>
+      </div>
+    </div>
+  </div>
+ 
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="icon-prev"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="icon-next"></span>
+  </a>
+</div> <!-- Carousel -->
--- boot/js/main.js
+++ boot/js/main.js
@@ -1,1 +1,6 @@
-main.js
+$(document).ready(function(){
+
+    $('.carousel').carousel({
+        interval: 3000
+    })
+});
